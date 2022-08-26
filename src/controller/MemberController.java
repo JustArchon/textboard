7e17209170672727cd91cd85e79a2f10de7ffa0d
JustArchon <puppy1900@naver.com>
2022-08-26 16:55:52 +0900

    Member information modify add

--- src/controller/MemberController.java
+++ src/controller/MemberController.java
@@ -6,6 +6,7 @@ import infra.Request;
 import service.MemberService;
 import utils.Util;
 
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class MemberController implements Controller{
@@ -31,6 +32,9 @@ public class MemberController implements Controller{
                 break;
             case "detail":
                 detail(request);
+                break;
+            case "modify":
+                modify(request);
                 break;
             default:
                 System.out.println("올바른 요청을 보내주시기 바랍니다.");
@@ -106,5 +110,33 @@ public class MemberController implements Controller{
         System.out.println("아이디 : " + loginId);
         System.out.println("이름 : " + findMember.getName());
         System.out.println("가입일 : " + findMember.getRegDate());
+    }
+    public void modify(Request request){
+        String paramKey = "loginId";
+        if(!Util.hasParam(request, paramKey)){
+            System.out.println(paramKey + " 파라미터가 필요합니다.");
+            return;
+        }
+
+        String logonMemberId = request.getLogonMemberId();
+        String parameterValue = request.getParameterStrValue(paramKey);
+
+        if(!logonMemberId.equals(parameterValue)){
+            System.out.println("본인 정보만 수정할 수 있습니다.");
+            return;
+        }
+
+        Member findMember = memberService.getMemberByLoginId(parameterValue);
+
+        System.out.print("변경하고자 하는 비밀번호 : ");
+        String newPassword = sc.nextLine().trim();
+        findMember.setPassword(newPassword);
+
+        System.out.print("변경하고자 하는 이름 : ");
+        String newName = sc.nextLine().trim();
+        findMember.setName(newName);
+
+        System.out.println("비밀번호가 잘 변경 되었습니다.");
+
     }
 }
